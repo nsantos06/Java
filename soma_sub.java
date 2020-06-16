225462e164327ecbe34ebdfcaab57e418c2bd50d
nsantos06 <51927883+nsantos06@users.noreply.github.com>
2020-06-15 21:04:53 -0300

    Update soma_sub.java

--- soma_sub.java
+++ soma_sub.java
@@ -3,18 +3,25 @@ class Main {
   public static void main(String[] args) {
   
   Scanner ler = new Scanner( System.in );
-  int n1,n2,soma,sub;
+  double n1,n2,soma,sub,div,mt;
 
   System.out.println("Digita um numero:");
-  n1 = ler.nextInt();
+  n1 = ler.nextDouble();
   System.out.println("Digita outro numero:");
-  n2 = ler.nextInt();
+  n2 = ler.nextDouble();
 
   soma = n1 + n2;
   System.out.println("A soma é:" + soma);
 
   sub = n1-n2;
-  System.out.println("A sub é:" + sub);
+  System.out.println("A subtracao é:" + sub);
+
+  div = n1/n2;
+  System.out.println("A divisao é:" + div);
+
+  mt = n1*n2;
+  System.out.println("A multiplicacao é:" + mt);
+
 
   }
 }
